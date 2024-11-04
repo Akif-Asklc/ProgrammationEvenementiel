ae860ec2375d0101195884ff541ceb978726b54d
Akif <80062851+Akif-Asklc@users.noreply.github.com>
2024-11-04 10:49:48 +0000

    Enreigistrement 04/11-11:49

--- Exception.py
+++ Exception.py
@@ -3,4 +3,21 @@ def divEntier(x: int, y: int) -> int:
         return 0
     else:
         x = x - y
-        return divEntier(x, y)
+        return divEntier(x, y) + 1
+    
+if __name__ == '__main__':
+    try:
+        try:
+           
+            x = -1
+            y = 2
+            if x < 0:
+                raise ValueError("La valeur négative.")
+            if x == 0:
+                raise ValueError("La valeur est égale à 0.")
+            print(divEntier(x, y))
+        except RecursionError:
+            print("La valeur 0 n'est pas accepté pour Y.")
+    except ValueError: 
+        print("Veuillez rentrer une valeur numérique.")
+
