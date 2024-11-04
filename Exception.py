231dea68c1d382378b7564d9aaedc4bd717e3438
Akif <80062851+Akif-Asklc@users.noreply.github.com>
2024-11-04 14:21:58 +0000

    Envoie

--- Exception.py
+++ Exception.py
@@ -6,15 +6,14 @@ def divEntier(x: int, y: int) -> int:
         return divEntier(x, y) + 1
     
 if __name__ == '__main__':
+    x = 1
+    y = 2
+    if x < 0:
+        raise ValueError("La valeur négative.")
+    if x == 0:
+        raise ValueError("La valeur est égale à 0.")
     try:
         try:
-           
-            x = -1
-            y = 2
-            if x < 0:
-                raise ValueError("La valeur négative.")
-            if x == 0:
-                raise ValueError("La valeur est égale à 0.")
             print(divEntier(x, y))
         except RecursionError:
             print("La valeur 0 n'est pas accepté pour Y.")
