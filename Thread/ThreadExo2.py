3961084c2970a0fc5c3ba74365972cb6a59c86d3
STF <80062851+Stikkife@users.noreply.github.com>
2024-11-14 23:32:00 +0100

    Rectification

--- Thread/ThreadExo2.py
+++ Thread/ThreadExo2.py
@@ -2,19 +2,10 @@ import threading
 import time
 
 # Création de la fonction Thread1
-def thread1(name, start):
-    count = start
+def thread(name, start):
+    count = start # Variable qui sert à compter le nb de seconde
     print(f"Numéro de départ de {name} : {count}")
-    while count > 0:
-        count -= 1
-        print(f"{name} : {count}")
-        time.sleep(1)
-        
-# Création de la fonction Thread2
-def thread2(name, start):
-   count = start
-   print(f"Numéro de départ de {name} : {count}")
-   while count > 0:
+    while count > 0: # Le TIMER
         count -= 1
         print(f"{name} : {count}")
         time.sleep(1)
@@ -22,14 +13,14 @@ def thread2(name, start):
 
 start = time.perf_counter()
 
-t1 = threading.Thread(target=thread1, args=("Thread1", 7)) # création de la thread1 | Target vise la fonction thread1 | l'argument vise 'i'
-t2 = threading.Thread(target=thread2, args=("Thread2", 5)) # création de la thread2 | Target vise la fonction thread2 | l'argument vise 'i'
+t1 = threading.Thread(target=thread, args=("Thread1", 7)) # création de la thread n°1 | Target vise la fonction thread1 | l'argument vise 'i'
+t2 = threading.Thread(target=thread, args=("Thread2", 5)) # création de la thread n°2 | Target vise la fonction thread2 | l'argument vise 'i'
 
-t1.start() # je démarre la thread
-t2.start() # je démarre la thread
+t1.start() # je démarre la première thread
+t2.start() # je démarre la deuxième thread
 
 
-t1.join() # attend la fin de la thread pour ne pas laissez le thread en arrière plan (mode zombie)
+t1.join() # attend la fin de la thread pour ne pas laissez la thread en arrière plan (mode zombie)
 t2.join()
 
 
